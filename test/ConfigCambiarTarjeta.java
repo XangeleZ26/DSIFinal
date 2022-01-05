0c7d91ad34e81cee435ec37e5dcfe7b6ee52c73f
VillanuevaZZ <GIGABYTE@DESKTOP-20IMHNT>
2022-01-05 17:32:47 -0500

    ControladorConfiguración semi terminado, para poder terminarlo, primero necesito que terminen el proceso de creación de cuenta y posterior login (se pasará el usuario logueado por parámetro a los distintos constructores que dependen del usuario (configuracion,opcionesIngreso, etc etc)

--- test/ConfigCambiarTarjeta.java
+++ test/ConfigCambiarTarjeta.java
@@ -18,7 +18,7 @@ public class ConfigCambiarTarjeta extends javax.swing.JFrame {
     public ConfigCambiarTarjeta() {
         initComponents();
     }
-
+//xd
     /**
      * This method is called from within the constructor to initialize the form.
      * WARNING: Do NOT modify this code. The content of this method is always
